7de7238fcc9d5757cda1d13bf33aaba6b6f4444b
Eddie Welker <eddie.welker@gmail.com>
2012-08-29 14:11:49 -0400

    changed info.py to support multiple environments

--- info.py
+++ info.py
@@ -1,14 +1,28 @@
-#!/home/welkere/env/dev_info/bin/python
-
 import sys, os
 
 sys.stderr = sys.stdout
 #print "Content-type: text/plain"
 #print
 
-os.environ['VIRTUAL_ENV'] = '/home/welkere/env/dev_info'
+homedir = '/home/welkere'
+#the name of the current directory is the same as the env name
+env_name =  os.path.basename(os.path.normpath(os.path.dirname(__file__)))
 
-venv = '/home/welkere/env/dev_info/bin/activate_this.py'
+venv_loc = homedir + '/env/' + env_name
+activate_loc = venv_loc + '/bin/activate_this.py'
+info_parent_loc = homedir + '/python'
+info_loc = info_parent_loc + '/' + env_name
+settings = env_name + '.settings'
+
+#print venv_loc
+#print activate_loc
+#print info_parent_loc
+#print info_loc
+#print settings
+
+os.environ['VIRTUAL_ENV'] = venv_loc 
+
+venv = activate_loc
 execfile(venv, dict(__file__=venv))
 
 _project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,17 +31,17 @@ _project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Add a custom Python path.
 sys.path.insert(0, _project_dir)
-sys.path.insert(0, '/home/welkere/python/dev_info')
+sys.path.insert(0, info_loc)
 #need to do this explicitly, since _project_dir is gonna be my staff directory,
 #because that's where the cgi is 'running'
-sys.path.insert(0, '/home/welkere/python')
+sys.path.insert(0, info_parent_loc)
 sys.path.insert(0, os.path.dirname(_project_dir))
 
 # Switch to the directory of your project. (Optional.)
-os.chdir("/home/welkere/python/dev_info")
+os.chdir(info_loc)
 
 # Set the DJANGO_SETTINGS_MODULE environment variable.
-os.environ['DJANGO_SETTINGS_MODULE'] = "dev_info.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = settings
 
 from django.core.servers.fastcgi import runfastcgi
 runfastcgi(method="threaded", daemonize="false")
